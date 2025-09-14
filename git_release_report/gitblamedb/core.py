ea8c43903dc5e40dcb2881cea39e291d603ee81d
tolatolatop <tolatolatop@gmail.com>
2025-09-14 01:35:42 +0000

    feat: 新增列出指定范围内提交的功能，并添加中文文档字符串

--- git_release_report/gitblamedb/core.py
+++ git_release_report/gitblamedb/core.py
@@ -234,3 +234,20 @@ def create_blame_repo(repo_path: str, regex: str, sess: Session):
     commits = [to_commit_model(repo_name, commit) for commit in commits]
 
     return file_cache, blame_lines, commits
+
+
+def list_commits(repo_path: str, rev_1: str, rev_2: str) -> List[CommitModel]:
+    """
+    列出仓库中的commit
+
+    Args:
+        repo_path: 仓库路径
+        rev_1: 第一个提交
+        rev_2: 第二个提交
+
+    Returns:
+        List[CommitModel]: commit模型列表
+    """
+    repo_name = os.path.basename(repo_path)
+    repo = Repo(repo_path)
+    return [to_commit_model(repo_name, commit) for commit in repo.iter_commits(f"{rev_1}..{rev_2}")]
