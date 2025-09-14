f66ca9c506f84a1a8b240f8266a69329f012dd7b
tolatolatop <tolatolatop@gmail.com>
2025-09-14 23:08:33 +0000

    feat: 获取同步信息

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -23,4 +23,5 @@ def test_compare_commits(repo_path, old_commit, new_commit):
     assert len(result['commits_df']) == 223
     # assert len(result.commits_stats_df) == 17718
     assert len(result['diff_info_df']) == 231
-    assert len(result['blame_lines_df']) == 466
+    # 大约2分钟
+    assert len(result['blame_lines_df']) == 543180
